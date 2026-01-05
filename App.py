ebf5c1ad974dcfd88f55788a86f66653f58925a5
abhinav4224-commits <abhinav4224@gmail.com>
2026-01-05 11:20:20 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pickle
+import os
+import numpy as np
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+SCALER_PATH = os.path.join(BASE_DIR, "scaler.pkl")
+
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+with open(SCALER_PATH, "rb") as f:
+    scaler = pickle.load(f)
